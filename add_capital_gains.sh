94f875f0a8f14d24c34809946306164db1190e61
Adam Harrington <ohingardail@hotmail.co.uk>
2014-08-20 16:34:34 +0100

    Create add_capital_gains.sh
    MySQL custom procedure 'post_all_gains' is defined in GitHub : ohingardail/gnucash-mysql-additions

---
+++ add_capital_gains.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# load parmfiles if exist
+if [ -d ../parms ]
+then
+for PARMFILE in `find ../parms -type f -executable -user ${USER} | sort`
+do 
+	if [ "${DEBUG}" == "ON" ]
+	then
+		${ECHO} "Loading parameter file : ${PARMFILE}"
+	fi
+	
+	. ${PARMFILE}
+done
+fi
+
+# check that mysql is running
+if [ `systemctl show mysqld.service | grep SubState= | awk -F= '{print $NF}'` != 'running' ]
+then
+	exit 1
+fi
+
+# add required capital gains or losses 
+mysql -s --user=${GNUCASH_USERNAME} --password=${GNUCASH_PASSWORD} --database=${CUSTOM_GNUCASH_DATABASE} <<-EOF 
+call post_all_gains();
+EOF
